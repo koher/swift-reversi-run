0e4910faeb1e7caf0b5cc76ea1d6b46a52b07eb7
Yuta Koshizawa <koher@koherent.org>
2021-07-24 18:01:46 +0900

    ArgumentParserの導入

--- Package.swift
+++ Package.swift
@@ -9,14 +9,15 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(name: "SwiftyReversi", url: "https://github.com/koher/swifty-reversi.git", from: "0.2.0-beta"),
         .package(url: "https://github.com/kareman/SwiftShell.git", from: "5.1.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.4.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swift-reversi-run",
-            dependencies: ["SwiftyReversi", "SwiftShell"],
-            exclude: ["run"]),
+            dependencies: ["SwiftyReversi", "SwiftShell", .product(name: "ArgumentParser", package: "swift-argument-parser")],
+            exclude: ["run", "run-docker"]),
         .testTarget(
             name: "swift-reversi-runTests",
             dependencies: ["swift-reversi-run"]),
--- Sources/swift-reversi-run/main.swift
+++ Sources/swift-reversi-run/main.swift
@@ -1,111 +1,118 @@
 import Foundation
 import SwiftyReversi
 import SwiftShell
+import ArgumentParser
 
-let arguments = CommandLine.arguments
-guard arguments.count == 3 else {
-    print("The number of arguments must be 3: CommandLine.arguments.count = \(arguments.count)")
-    exit(1)
-}
-
-let programID1: String = arguments[1]
-let programID2: String = arguments[2]
-
-var game: Game = .init()
-var moveCount: Int = 1
-
-let runPath: String = ((#file as NSString).deletingLastPathComponent as NSString).appendingPathComponent("run")
-
-print(game.board.description)
-print()
-print("x: \(game.board.count(of: .dark)), o: \(game.board.count(of: .light))")
-print()
-print("-----")
-print()
-
-while case .beingPlayed(let turn) = game.state {
-    print("\(moveCount): \(turn)")
-    print()
+struct Command: ParsableCommand {
+    @Argument(help: "黒でプレイするAIのコードが書かれたswiftファイルのパス。\"manual\"を指定するとマニュアルでプレイできる。")
+    var dark: String
     
-    let programID: String
-    let board: Board
-    switch turn {
-    case .dark:
-        programID = programID1
-        board = game.board
-    case .light:
-        programID = programID2
-        board = game.board.flipped()
-    }
-    if board.validMoves(for: .dark).isEmpty {
-        try! game.pass()
-        continue
-    }
+    @Argument(help: "白でプレイするAIのコードが書かれたswiftファイルのパス。\"manual\"を指定するとマニュアルでプレイできる。")
+    var light: String
     
-    if programID == "manual" {
-        while true {
-            let rawOutput = readLine()!
-            
-            print("output:", rawOutput)
-            let components = rawOutput.split(separator: " ")
-            guard components.count == 2, let x = Int(components[0]), let y = Int(components[1]) else {
-                print("Illegal output")
-                continue
-            }
-            
-            do {
-                try game.placeDiskAt(x: x, y: y)
-            } catch {
-                print("Illegal move")
-                continue
-            }
-            
-            break
-        }
-    } else {
-        let result = run(runPath, [programID, board])
-        
-        guard result.exitcode == 0 else {
-            print(result.stderror)
-            print("Winner: \(turn.flipped)")
-            break
-        }
-        
-        let rawOutput = result.stdout
-        
-        print("output:", rawOutput)
-        let components = rawOutput.split(separator: " ")
-        guard components.count == 2, let x = Int(components[0]), let y = Int(components[1]) else {
-            print("Illegal output")
-            print("Winner: \(turn.flipped)")
-            break
-        }
-        
-        do {
-            try game.placeDiskAt(x: x, y: y)
-        } catch {
-            print("Illegal move")
-            print("Winner: \(turn.flipped)")
-            break
-        }
-    }
+    @Option(name: .long, help: "Docker上で安全に対戦させる。")
+    var docker: Bool = false
 
-    print()
-    print(game.board.description)
-    print()
-    print("x: \(game.board.count(of: .dark)), o: \(game.board.count(of: .light))")
-    print()
-    
-    if case .over(let winner) = game.state {
-        if let winner = winner {
-            print("Winner: \(winner)")
-        } else {
-            print("Draw")
-        }
-    } else {
+    func run() throws {
+        var game: Game = .init()
+        var moveCount: Int = 1
+
+        let runPath: String = ((#file as NSString).deletingLastPathComponent as NSString).appendingPathComponent("run")
+
+        print(game.board.description)
+        print()
+        print("x: \(game.board.count(of: .dark)), o: \(game.board.count(of: .light))")
+        print()
         print("-----")
         print()
-    }
 
-    moveCount += 1
+        while case .beingPlayed(let turn) = game.state {
+            print("\(moveCount): \(turn)")
+            print()
+            
+            let player: String
+            let board: Board
+            switch turn {
+            case .dark:
+                player = dark
+                board = game.board
+            case .light:
+                player = light
+                board = game.board.flipped()
+            }
+            if board.validMoves(for: .dark).isEmpty {
+                try! game.pass()
+                continue
+            }
+            
+            if player == "manual" {
+                while true {
+                    let rawOutput = readLine()!
+                    
+                    print("output:", rawOutput)
+                    let components = rawOutput.split(separator: " ")
+                    guard components.count == 2, let x = Int(components[0]), let y = Int(components[1]) else {
+                        print("Illegal output")
+                        continue
+                    }
+                    
+                    do {
+                        try game.placeDiskAt(x: x, y: y)
+                    } catch {
+                        print("Illegal move")
+                        continue
+                    }
+                    
+                    break
+                }
+            } else {
+                let result = SwiftShell.run(runPath, [player, board])
+                
+                guard result.exitcode == 0 else {
+                    print(result.stderror)
+                    print("Winner: \(turn.flipped)")
+                    break
+                }
+                
+                let rawOutput = result.stdout
+                
+                print("output:", rawOutput)
+                let components = rawOutput.split(separator: " ")
+                guard components.count == 2, let x = Int(components[0]), let y = Int(components[1]) else {
+                    print("Illegal output")
+                    print("Winner: \(turn.flipped)")
+                    break
+                }
+                
+                do {
+                    try game.placeDiskAt(x: x, y: y)
+                } catch {
+                    print("Illegal move")
+                    print("Winner: \(turn.flipped)")
+                    break
+                }
+            }
+
+            print()
+            print(game.board.description)
+            print()
+            print("x: \(game.board.count(of: .dark)), o: \(game.board.count(of: .light))")
+            print()
+            
+            if case .over(let winner) = game.state {
+                if let winner = winner {
+                    print("Winner: \(winner)")
+                } else {
+                    print("Draw")
+                }
+            } else {
+                print("-----")
+                print()
+            }
+
+            moveCount += 1
+        }
+    }
 }
+
+Command.main()
